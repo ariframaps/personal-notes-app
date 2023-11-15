7fe56e2891810816de0e6f6307e518291311e3bf
ariframaps <ariframa@student.ub.ac.id>
2023-11-16 06:26:26 +0700

    membuat component header

---
+++ src/components/NoteAppHeader.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+class NoteAppHeader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: ''
+        }
+
+        this.onSearchChangeHandler = this.onSearchChangeHandler.bind(this);
+    }
+
+    onSearchChangeHandler(event) {
+        this.setState(() => {
+            return {
+                search: event.target.value
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="note-app__header">
+                <h1>Notes</h1>
+                <input type="text" className="note-search" onChange={this.onSearchChangeHandler} placeholder="Cari catatan..." />
+            </div>
+        );
+    }
+}
+
+export default NoteAppHeader;
